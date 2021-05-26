d8e8893acba89077a6ec301bf1aae778e1a3d778
Sameer Padala <sameer@padalas.net>
2021-05-25 19:10:42 -0700

    WIP with macros and namespaces

--- src/repl/core.cljs
+++ src/repl/core.cljs
@@ -2,8 +2,11 @@
   (:require
     [sicmutils.env :as env :include-macros true]
     [sicmutils.expression.render :as render :refer [->TeX]]
-    [cljs.js :refer [empty-state eval-str js-eval]]
-    [cljs.repl :as repl]))
+    [cljs.js :refer [compile-str empty-state eval-str js-eval]]
+    [cljs.repl :as repl]
+    [repl.lib])
+  (:require-macros [repl.analysis :refer [analyzer-state]])
+)
 ;(env/bootstrap-repl!)
 
 (defn asin [x] (sicmutils.env/asin x))
@@ -17,23 +20,52 @@
 (defn up [x y] (sicmutils.env/up x y))
 (defn + [x y] (sicmutils.env/+ x y))
 
+(def S sicmutils.env)
+
 (defn pTeX [ex]
-  (let [s (->TeX (simplify ex))]
+  (let [s (->TeX (env/simplify ex))]
     (js/outputTex s)))
+
     
+
+(defn dummy-loader  [opts cb]
+  (println "(dummy-loader " opts ")")
+  (cb {:lang :clj :source "(println \"dummy-loader cb\")"}))
+
 (def state (cljs.js/empty-state))
-(defn ^:export evalStr [source, cb]
-  (eval-str state
-        source
-        "evalStr"
-        { :context    :expr
+(def opts { :context    :expr
           :def-emits-var true
           :eval       js-eval
+          :load       dummy-loader
           :ns 'repl.core
-          :source-map true}
-          (fn [result] (cb (clj->js result)))))
+          :source-map true})
+(defn ^:export evalStr [source, cb]
+  (compile-str state source "evalStr" opts println)
+  (eval-str state source "evalStr" opts (fn [result] (cb (clj->js result)))))
 
+;(cljs.js/load-analysis-cache! state 'repl.lib (analyzer-state 'repl.lib))
+(cljs.js/load-analysis-cache! state 'repl.core (analyzer-state 'repl.core))
+;(cljs.js/load-analysis-cache! state 'sicmutils.abstract.function (analyzer-state 'sicmutils.abstract.function))
+;(cljs.js/load-analysis-cache! state 'sicmutils.env (analyzer-state 'sicmutils.env))
 (js/log "CLJS loading complete.");
+;(js/console.log (type sicmutils.env/sin))
+;(js/console.log (type sicmutils.generic/sin))
+;(js/console.log (type sin))
+
+
+;(js/console.log  sicmutils.env/sin)
+;(js/console.log  sicmutils.generic/sin)
+;(js/console.log  sin)
+
+;(js/console.log (type repl.lib/libF))
+;(js/console.log  repl.lib/libF)
+;(js/console.log  (repl.lib/libF 1));
+
+;(js/console.log (type repl.lib/libM))
+;(js/console.log  repl.lib/libM)
+;(js/console.log  (repl.lib/libM 1))
+;(js/console.log  (repl.lib/libM 1 2))
+
 
 (comment
   (pTeX (- (* 7 (/ 1 2)) 2))
