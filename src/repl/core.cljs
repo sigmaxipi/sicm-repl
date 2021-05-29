8b10a506ee88de2f09726d0792e659a2d98dad92
Sameer Padala <sameer@padalas.net>
2021-05-29 01:08:02 -0700

    Improved macro support

--- src/repl/core.cljs
+++ src/repl/core.cljs
@@ -4,6 +4,7 @@
     [cljs.js :refer [compile-str empty-state eval-str js-eval]]
     [cljs.reader]
     [cljs.tools.reader.reader-types]
+    [sicmutils.abstract.function]
     [sicmutils.env :as env :include-macros true]
     [sicmutils.expression.render :as render :refer [->TeX]])
   (:require-macros [repl.macros] [sicmutils.env])
@@ -11,19 +12,20 @@
 
 (repl.macros/bootstrap-env!)
 
-; Output strings to Javascript side.
-(defn warning-handler [warning-type env extra]
-  (js/logW (str (cljs.analyzer/message env (str "WARNING: " (cljs.analyzer/error-message warning-type extra))))))
-(set! cljs.analyzer/*cljs-warning-handlers* [warning-handler])
-
 (defn pTeX [ex]
   (let [s (->TeX (simplify ex))]
     (js/outputTex s)))
 
 ; Allow (require sicmutils.env) to work.
 (defn loader  [opts cb]
-  (println "(dummy-loader " opts ")")
-    (cb {:lang :clj :source (repl.macros/overrideCore)}))
+  (if (= (:name opts) 'repl.macroEval)
+    (do
+      (println "(repl.macroEval loader " opts ")" (:name opts))
+      (cb {:lang :clj :source (repl.macros/loadReplMacro)}))
+    (do
+      (println "(dummy-loader " opts ")" (:name opts))
+      (cb {:lang :clj :source ""}))
+  ))
 
 ; Break up the input so that it can be evaluated one form at a time.
 ; This allows for better error handling and extraction of the final value.
@@ -39,6 +41,7 @@
 
 ; Actual eval code.
 (def state (cljs.js/empty-state))
+
 (defn ^:export evalStr [source, compile-cb, eval-cb]
   ; Compile the entire source to check for syntax errors.
   (compile-str
@@ -76,5 +79,30 @@
             ; Send final value if we didn't error out.
             (if-not (:error @*eval-result*) (eval-cb "" (clj->js @*eval-result*))))))))
 
+; Load 'repl.core symbols into the evalution state.
 (cljs.js/load-analysis-cache! state 'repl.core (repl.macros/analyzer-state 'repl.core))
+
+; Reinit 'repl.core as an evaulation namespace.
+(defn init-state [state] 
+  (eval-str
+    state "
+      (ns repl.core
+        (:require [sicmutils.abstract.function]
+                  [sicmutils.env :as S] )
+        (:require-macros [repl.macroEval :refer [literal-function]]))
+      (repl.macroEval/overrideCore)"
+    "initialEvalStr"
+    { :context    :statement
+      :eval       js-eval
+      :load       loader
+      :ns 'repl.core
+      :source-map true}
+    js/console.log))
+(init-state state)
+
+; Output strings to Javascript side.
+(defn warning-handler [warning-type env extra]
+  (js/logW (str (cljs.analyzer/message env (str "WARNING: " (cljs.analyzer/error-message warning-type extra))))))
+(set! cljs.analyzer/*cljs-warning-handlers* [warning-handler])
+
 (js/log "...CLJS loading complete.")
--- src/repl/macros.clj
+++ src/repl/macros.clj
@@ -23,15 +23,26 @@
       `(defn ~v [& ~'xs] (apply ~(symbol "sicmutils.env" (str v)) ~'xs)))))
 
 (defmacro bootstrap-env! [] '(do
-  (repl.macros/importUnary [literal-function])
-  (repl.macros/importVariadic [asin atan compose cos cube D F->C Gamma Lagrange-equations simplify sin square up velocity + - * /])))
+  (repl.macros/importUnary [])
+  (repl.macros/importVariadic [asin atan compose cos cube D F->C Gamma Lagrange-equations simplify sin square up velocity])))
 
 ; If (ns ...) (require ...) is called in the evaluation snippet, the math operators are overridden. This restores the bindings.
-(defmacro overrideCore []
-  "(ns repl.macro)
+(defmacro loadReplMacro []
+  "(ns repl.macroEval)
     (defmacro overrideCore []
       '(do
         (ns-unmap 'cljs.core '+) (ns-unmap 'repl.core '+) (def + sicmutils.env/+)
                                  (ns-unmap 'repl.core '-) (def - sicmutils.env/-)
         (ns-unmap 'cljs.core '*) (ns-unmap 'repl.core '*) (def * sicmutils.env/*)
-        (ns-unmap 'cljs.core '/) (ns-unmap 'repl.core '/) (def / sicmutils.env//)))")
+        (ns-unmap 'cljs.core '/) (ns-unmap 'repl.core '/) (def / sicmutils.env//)))
+        
+        
+  (defmacro literal-function
+    ([f] `(sicmutils.abstract.function/literal-function ~f))
+    ([f sicm-signature]
+    (if (and (list? sicm-signature)
+              (= '-> (first sicm-signature)))
+      `(sicmutils.abstract.function/literal-function ~f '~sicm-signature)
+      `(sicmutils.abstract.function/literal-function ~f ~sicm-signature)))
+    ([f domain range] `(sicmutils.abstract.function/literal-function ~f ~domain ~range)))           
+        ")
